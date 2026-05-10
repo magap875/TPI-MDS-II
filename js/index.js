a6dc1d2f7d45aeaa5f111a3744a3b8698331f7de
magap875 <135629818+magap875@users.noreply.github.com>
2026-05-10 12:31:38 -0300

    baja de productos y configurar stock minimo

--- js/index.js
+++ js/index.js
@@ -56,7 +56,8 @@ function mostrarProductos(productos) {
 
 async function cargarYRenderizar() {
     productosGlobal = await obtenerProductos();
-    mostrarProductos(productosGlobal);
+    const productosActivos = productosGlobal.filter(p => p.activo !== false);
+    mostrarProductos(productosActivos);
 }
 
 function activarBotonesCarrito() {
@@ -134,6 +135,7 @@ if (inputFiltro) {
         const texto = inputFiltro.value.trim().toLowerCase();
 
         const filtrados = productosGlobal.filter(p =>
+            p.activo !== false &&
             p.nombre.toLowerCase().includes(texto)
         );
 
